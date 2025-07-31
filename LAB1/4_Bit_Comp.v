e961ad57d6f115e944f55a15d10dfbea9923e3d4
Sanket-IITG <sanketravindradaduria@gmail.com>
2025-07-31 13:20:25 +0530

    Create 4_Bit_Comp.v

---
+++ LAB1/4_Bit_Comp.v
@@ -0,0 +1,9 @@
+module 4biteqcomp(
+  input [3:0] A; //4 Bit
+  input [3:0] B; //4 Bit
+  output C; // output of 1 bit
+); //module instantiation
+ 
+C = (A == B) ? 1 : 0; // Conditional operator: assigns 1 to C if all 3 bits of A are equal to all 3 bits of B; otherwise, assigns 0.
+
+endmodule //end of module
